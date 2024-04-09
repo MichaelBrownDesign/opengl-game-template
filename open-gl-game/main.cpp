0bec5ec539936a2a21e9aea1e600eec0861e95ba
b-side-dev <b.side.org@gmail.com>
2024-04-09 18:23:51 +1000

    chore: added libs to cmake, added include headers

--- open-gl-game/main.cpp
+++ open-gl-game/main.cpp
@@ -1,9 +1,16 @@
 ﻿#include <iostream>
 
+#define NO_SDL_GLEXT
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#undef main
+
 using namespace std;
 
 int main()
 {
 	cout << "Hello OpenGL!" << endl;
+
 	return 0;
 }
